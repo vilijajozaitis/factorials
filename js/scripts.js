dbdb4984468b685476b26d57b529e227566a300c
vilijajozaitis <48165585+vilijajozaitis@users.noreply.github.com>
2019-06-19 19:57:48 -0700

    Add files via upload
    added user input to function with dom

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 var factorialize = function(num) {
   if (num === 0 || num === 1)
     return 1;
-    // console.log(1);
+  else if (num >= 6)
+    return ("Enter a number less than 6")
+
   for (var i = num - 1; i >= 1; i--) {
     num *= i;
   }
   return num;
-  // console.log(num);
+
 }
 
 $(document).ready(function() {
   $("form#factorialize").submit(function(event) {
     event.preventDefault();
-    // var input = $("#i").val();
-    var result = factorialize(5);
-    console.log(result);
-    });
+    var entry = parseInt($("input#digit").val());
+    var result = factorialize(entry)
+  
+      $(".entry").text(result);
+      $(".originalNumber").text(entry);
+
+
+    $("#result").show("#result");
   });
+});
